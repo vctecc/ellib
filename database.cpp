eca17cf824d7093f7d9464989b8c277e3af031e8
vctecc <vctecc@gmail.com>
2018-05-01 16:10:37 +0300

    Added createTable, insertIntoTable, removeRecord, restoreDataBase

--- database.cpp
+++ database.cpp
@@ -1,6 +1,69 @@
 #include "database.h"
 
-database::database()
+DataBase::DataBase(QObject *parent) : QObject(parent)
 {
 
+}
+
+DataBase::~DataBase()
+{
+
+}
+
+/* Методы для подключения к базе данных
+ * */
+void DataBase::connectToDataBase()
+{
+    /* Перед подключением к базе данных производим проверку на её существование.
+     * В зависимости от результата производим открытие базы данных или её восстановление
+     * */
+    if(!QFile("D:\\Work\\DataBase.db" ).exists()){
+        this->restoreDataBase();
+    } else {
+        this->openDataBase();
+    }
+}
+
+/* Методы восстановления базы данных
+ * */
+bool DataBase::restoreDataBase()
+{
+    return true;
+}
+
+/* Метод для открытия базы данных
+ * */
+bool DataBase::openDataBase()
+{
+    /* База данных открывается по заданному пути
+     * и имени базы данных, если она существует
+     * */
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("D:\\Work\\DataBase.db");
+    if(db.open()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Методы закрытия базы данных
+ * */
+void DataBase::closeDataBase()
+{
+    db.close();
+}
+
+/* Метод для создания таблицы в базе данных
+ * */
+bool DataBase::createTable()
+{
+    return true;
+}
+
+/* Метод для вставки записи в базу данных
+ * */
+bool DataBase::inserIntoTable(const QVariantList &data)
+{
+    return true;
 }
--- database.h
+++ database.h
@@ -1,11 +1,46 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QObject>
+#include <QSql.h>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlDatabase>
+#include <QFile>
+#include <QDate>
+#include <QDebug>
 
-class database
+
+/* Директивы имен таблицы, полей таблицы и базы данных */
+#define DATABASE_HOSTNAME   "ExampleDataBase"
+#define DATABASE_NAME       "DataBase.db"
+
+#define TABLE                   "Example"
+#define TABLE_DATE              "Date"
+#define TABLE_TIME              "Time"
+#define TABLE_MESSAGE           "Message"
+#define TABLE_RANDOM            "Random"
+
+class DataBase : public QObject
 {
 public:
-    database();
+    explicit DataBase(QObject *parent = 0);
+    ~DataBase();
+    /* Методы для непосредственной работы с классом
+     * Подключение к базе данных и вставка записей в таблицу
+     * */
+    void connectToDataBase();
+    bool inserIntoTable(const QVariantList &data);
+
+private:
+    QSqlDatabase db;
+
+
+private:
+    bool openDataBase();
+    bool restoreDataBase();
+    void closeDataBase();
+    bool createTable();
 };
 
 #endif // DATABASE_H
