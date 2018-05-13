f2940a678d956db7bfabf68a5f1851d27aaf314d
vctecc <vctecc@gmail.com>
2018-05-13 21:33:22 +0300

    The document settings are set globally as strings

--- libraryclient.cpp
+++ libraryclient.cpp
@@ -1,20 +1,241 @@
 #include <QtWidgets>
 #include "libraryclient.h"
-
+#include "filterwindow.h"
+#include "editdocwindow.h"
+#include "newdocwindow.h"
+#include "setting.h"
 LibraryClient::LibraryClient(QWidget *parent) : QWidget(parent)
 {
-    label = new QLabel(tr("Hello world"));
-    model = new QStandardItemModel(10, 6, this);
+    setting = new Setting();
+    db = new DataBase();
+    db->connectToDataBase();
 
-    tabel = new QTableView(this);
-    tabel->setModel(model);
-    tabel->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    tabel->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+    this->setupModel(TABLE,
+                     QStringList() << trUtf8("Рег. №")
+                                   << trUtf8("Происхождение")
+                                   << trUtf8("Процесс")
+                                   << trUtf8("Категория")
+                                   << trUtf8("Имя документа"));
 
-    model->setHeaderData(0, Qt::Horizontal, QObject::tr("Рег. №"));
-    model->setHeaderData(1, Qt::Horizontal, QObject::tr("Происхождение"));
-    model->setHeaderData(2, Qt::Horizontal, QObject::tr("Процесс"));
-    model->setHeaderData(3, Qt::Horizontal, QObject::tr("Статус"));
-    model->setHeaderData(3, Qt::Horizontal, QObject::tr("Имя документа"));
+    //Адский костыль
+    sources = new QString();
+    sources->append(tr("Внешний,"
+                       "Внутренний"));
+
+    processes = new QString();
+    processes->append(tr("Заключение договоров,"
+                         "Проектирование и разработка,"
+                         "Закупки комплектующих и материалов,"
+                         "Производство продукции,"
+                         "Контроль и испытание,"
+                         "Хранение и поставка,"
+                         "Обеспечение средствами производства,"
+                         "Обеспечение средствами контроля,"
+                         "Обеспечение персоналом,"
+                         "Обеспечение информацией,"
+                         "Обеспечение иными ресурсами,"
+                         "Планирование действий,"
+                         "Документирование процедур,"
+                         "Распределение ответственности,"
+                         "Анализ со стороны руководства,"
+                         "Улучшение результативности"));
+
+    categories = new QString();
+    categories->append("Положение,"
+                       "Инструкция,"
+                       "Журнал,"
+                       "Технология,"
+                       "Ведомость,"
+                       "Стандарт,"
+                       "Карта,"
+                       "Пооперационная ведомость,"
+                       "Перечень,"
+                       "Регламент,"
+                       "Руководство,"
+                       "Технические условия,"
+                       "Спецификация");
+
+    status = new QString();
+    status->append("Действующий,"
+                   "Отменённый");
+
+    //createDocBar();
+    createUI();
 }
 
+LibraryClient::~LibraryClient()
+{
+
+}
+
+void LibraryClient::setupModel(const QString &tableName, const QStringList &headers)
+{
+    /* Производим инициализацию модели представления данных
+     * с установкой имени таблицы в базе данных, по которому
+     * будет производится обращение в таблице
+     * */
+    model = new QSqlTableModel(this);
+    model->setTable(tableName);
+    model->select(); // Делаем выборку данных из таблицы
+
+    /* Устанавливаем названия колонок в таблице с сортировкой данных
+     */
+    for(int i = 0, j = 0; i < model->columnCount(); i++, j++){
+        model->setHeaderData(i,Qt::Horizontal,headers[j]);
+    }
+    // Устанавливаем сортировку по возрастанию данных по нулевой колонке
+    model->setSort(0,Qt::AscendingOrder);
+    model->select(); // Делаем выборку данных из таблицы
+}
+
+void LibraryClient::createUI()
+{
+    QHBoxLayout *layout;
+    QGroupBox *documentGroupBox = new QGroupBox;
+
+    QToolButton *newButton = new QToolButton;
+    QToolButton *editButton = new QToolButton;
+    QToolButton *deleteButton = new QToolButton;
+
+    newButton->setText("Новый документ");
+    editButton->setText("Редактировать документ");
+    deleteButton->setText("Удалить документ");
+
+    connect(newButton, SIGNAL(clicked()), this, SLOT(newDocument()));
+    //connect(editButton, SIGNAL(clicked()), this, SLOT(editDocument()));
+    connect(deleteButton, SIGNAL(clicked()), this, SLOT(deleteDocument()));
+
+    layout = new QHBoxLayout;
+    layout->addWidget(newButton);
+    layout->addWidget(editButton);
+    layout->addWidget(deleteButton);
+    documentGroupBox->setLayout(layout);
+
+    QGroupBox *filterGroupBox = new QGroupBox;
+
+    QToolButton *turnOnFilterButton = new QToolButton;
+    QToolButton *turnOffFilterButton = new QToolButton;
+    QToolButton *extendFilterButton = new QToolButton;
+
+    turnOnFilterButton->setText("Фильтр по выделеному вкл");
+    turnOffFilterButton->setText("Фильтр по выделеному выкл");
+    extendFilterButton->setText("Расширенный фильтр");
+
+    connect(turnOnFilterButton, SIGNAL(clicked()), this, SLOT(filterTurnOn()));
+    connect(turnOffFilterButton, SIGNAL(clicked()), this, SLOT(filterTurnOff()));
+    connect(extendFilterButton, SIGNAL(clicked()), this, SLOT(extendFilter()));
+
+    layout = new QHBoxLayout;
+    layout->addWidget(turnOnFilterButton);
+    layout->addWidget(turnOffFilterButton);
+    layout->addWidget(extendFilterButton);
+    filterGroupBox->setLayout(layout);
+
+
+
+    table = new QTableView(this);
+    table->setRowHeight(100, 100);
+    table->setModel(model);     // Устанавливаем модель на TableView
+    // Разрешаем выделение строк
+    //table->setSelectionBehavior(QAbstractItemView::SelectRows);
+    // Устанавливаем режим выделения лишь одно строки в таблице
+    table->setSelectionMode(QAbstractItemView::SingleSelection);
+    // Устанавливаем размер колонок по содержимому
+    table->resizeColumnsToContents();
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    table->horizontalHeader()->setStretchLastSection(true);
+
+    QGridLayout *mainLayout = new QGridLayout;
+    mainLayout->addWidget(documentGroupBox, 0, 0);
+    mainLayout->addWidget(filterGroupBox, 1, 0);
+    mainLayout->addWidget(table, 2, 0);
+
+    setLayout(mainLayout);
+    setWindowTitle(tr("Электронная библиотека"));
+    // Открытие окна редактирования по двойному клику по документу
+    connect(table, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(slotEditDocument(QModelIndex)));
+}
+
+
+void LibraryClient::newDocument()
+{
+    QStringList sourcesList;
+    QStringList processesList;
+    QStringList categoriesList;
+
+    sourcesList << sources->split(',');
+    processesList <<processes->split(',');
+    categoriesList << categories->split(',');
+
+    /* Создаем диалог и подключаем его сигнал завершения работы
+    * к слоту обновления вида модели представления данных
+    * */
+    NewDocWindow *creator = new NewDocWindow(sourcesList, processesList, categoriesList);
+    connect(creator, SIGNAL(signalRegister()), this, SLOT(slotUpdateModels()));
+    creator->show();
+}
+
+void LibraryClient::slotUpdateModels()
+{
+    qDebug()<<"SetUpModel";
+    model->select();
+}
+
+void LibraryClient::editDocument()
+{
+
+    QStringList sources;
+    //QStringList processes;
+    QStringList categories;
+    QStringList status;
+
+    sources << tr("Внешний") << tr("Внутренний");
+    //processes << tr("Производство") << tr("Менеджмент");
+    categories << tr("ГОСТ") << tr("Документы предприятия");
+    status << tr("Действующий") << tr("Отменённый");
+
+   // EditDocWindow *editor = new EditDocWindow(sources, processes, categories, status);
+    //editor->show();
+}
+
+void LibraryClient::slotEditDocument(QModelIndex index)
+{
+    QStringList sourcesList;
+    QStringList processesList;
+    QStringList categoriesList;
+    QStringList statusList;;
+
+
+    sourcesList << sources->split(',');
+    processesList << processes->split(',');
+    categoriesList << categories->split(',');
+    statusList << status->split(',');
+
+    EditDocWindow *editWindow = new EditDocWindow(sourcesList, processesList,
+                                                  categoriesList, statusList, index.row());
+    connect(editWindow, SIGNAL(signalReady()), this, SLOT(slotUpdateModels()));
+
+    editWindow->show();
+
+}
+
+void LibraryClient::deleteDocument()
+{
+
+}
+
+void LibraryClient::filterTurnOn()
+{
+
+}
+
+void LibraryClient::filterTurnOff()
+{
+
+}
+
+void LibraryClient::extendFilter()
+{
+    FilterWindow *filter = new FilterWindow();
+    filter->show();
+}
--- libraryclient.h
+++ libraryclient.h
@@ -1,13 +1,20 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QMainWindow>
 #include <QWidget>
 #include <QTableView>
 #include <QStandardItemModel>
+#include <QSqlTableModel>
+
+#include <database.h>
+#include "setting.h"
 
 QT_BEGIN_NAMESPACE
 class QMenu;
 class QLabel;
+class QStringListModel;
+class QStringList;
 QT_END_NAMESPACE
 
 class LibraryClient : public QWidget
@@ -15,13 +22,36 @@ class LibraryClient : public QWidget
     Q_OBJECT
 
 public:
-    LibraryClient(QWidget *parent = 0);
+    explicit LibraryClient(QWidget *parent = 0);
+    ~LibraryClient();
 
+private slots:
+
+    void newDocument();
+    void editDocument();
+    void slotUpdateModels();
+    void slotEditDocument(QModelIndex index);
+    void deleteDocument();
+
+    void filterTurnOn();
+    void filterTurnOff();
+    void extendFilter();
 
 private:
-    QLabel *label;
-    QTableView *tabel;
-    QStandardItemModel *model;
+
+    QTableView *table;
+    DataBase *db;
+    QSqlTableModel *model;
+    Setting *setting;
+    //от это костыля нужно избавиться
+    QString *sources;
+    QString *processes;
+    QString *categories;
+    QString *status;
+
+private:
+    void setupModel(const QString &tableName, const QStringList &headers);
+    void createUI();
 };
 
 #endif
